37d23691ac8a0371acbc2de7b4f1a699ac17c5ed
Ilya <ilyakatasonov@gmail.com>
2022-11-19 17:25:43 +0300

    Task_6 was born

---
+++ HomeWork/Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число");
+string input1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+
+if ((number1 % 2) ==0 )
+{
+Console.WriteLine("ДА - число чётное");
+}
+
+else
+{
+Console.WriteLine("НЕТ - число не чётное");
+}
