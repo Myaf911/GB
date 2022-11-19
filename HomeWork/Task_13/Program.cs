bd07a3b9c59df68927004e508e55ecace5a7d5e9
Ilya <ilyakatasonov@gmail.com>
2022-11-19 18:16:29 +0300

    Task_13 final

---
+++ HomeWork/Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Задача 13");
+Console.WriteLine("Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.WriteLine("645 - > 5");
+Console.WriteLine("78 - > третьего числа нет");
+Console.WriteLine("32679 - > 6");
+Console.WriteLine(" ");
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> тут всего 2 числа, где третье???");
+}
