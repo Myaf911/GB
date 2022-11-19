5da73c98fa9406782959bd24138c7808f491bd29
Ilya <ilyakatasonov@gmail.com>
2022-11-19 17:02:00 +0300

    практическое_задание_1.md и Task_2 was born

---
+++ HomeWork/Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("введите число 1");
+string input1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+
+Console.WriteLine("введите число 2");
+string input2 = Console.ReadLine();
+int number2 = Convert.ToInt32(input2);
+
+if (number1 > number2)
+{
+Console.WriteLine("Число " + number1 + " больше");
+}
+
+if (number1 < number2)
+{
+Console.WriteLine("Число " + number2 + " больше");
+}
+
+if (number1 == number2) 
+{
+Console.WriteLine("Вы ввели одинаковые числа");
+}
