e9b25f57e1bd7092563274c4f8df060532573730
Ilya <ilyakatasonov@gmail.com>
2022-11-19 17:31:07 +0300

    Task_8 was born. Все задачи по Первому практическому заданию выполнены

--- HomeWork/Task_8/Program.cs
+++ HomeWork/Task_8/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите число");
+string input1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+int count = 2;
+
+Console.Write("Все чётные числа до вашего числа: ");
+
+while (count <= number1)
+{
+Console.Write(count + " ");
+count = count + 2;
+}
