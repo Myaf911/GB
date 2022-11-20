c0137dd94221591d8710cb72909d9511a916ca80
Ilya <ilyakatasonov@gmail.com>
2022-11-20 17:53:16 +0300

    final

---
+++ HomeWork/Task_23/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine("Задача 23");
+Console.WriteLine("Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+Console.WriteLine("3 - > 1, 8, 27");
+Console.WriteLine("5 - > 1, 8, 27, 64, 125");
+Console.WriteLine(" ");
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 1;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+Console.WriteLine("А вот и числа!!!");
+PrintArry(arry);
