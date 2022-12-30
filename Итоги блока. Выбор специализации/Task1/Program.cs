d2d2c86e0d7ea9c3aa25f48e4f185398723f31aa
Ilya <ilyakatasonov@gmail.com>
2022-12-30 19:31:04 +0300

    Final task v1

---
+++ Итоги блока. Выбор специализации/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите длину массива строк ");
+int n = Convert.ToInt32(ReadLine());
+
+string[] array = new string[n];
+string[] arrayResult = new string[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Write("Строка " + (i + 1) + ": ");
+    array[i] = ReadLine();
+}
+
+WriteLine("\nИсходный массив строк: ");
+
+for (int i = 0; i < array.Length; i++)
+{  
+    Write($"{array[i]} ");   
+}
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        arrayResult[count] = array[i];
+        count++;
+    }
+}
+
+WriteLine("\nИтоговый массив строк в котором длинна слов меньше или равна 3 символа: ");
+
+for (int i = 0; i < arrayResult.Length; i++)
+{  
+    Write($"{arrayResult[i]} ");   
+}
